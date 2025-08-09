d1127ef03a7598bc785c9b983510d2f99d502f86
ASWANI K A <aswanika05@gmail.com>
2025-08-09 20:11:59 +0530

    Add event handling to ProductList to get the product on clicking of Add to Card button

--- script.js
+++ script.js
@@ -25,4 +25,12 @@ document.addEventListener("DOMContentLoaded", () => {
         `;
         productList.appendChild(productDiv);
     });
+
+    productList.addEventListener("click", (e) => {
+        if(e.target.tagName === "BUTTON") {
+            const productId = parseInt(e.target.getAttribute("data-id"));
+            const product = products.find((p) => p.id === productId);
+            addToCart(product);
+        }
+    });
 });
